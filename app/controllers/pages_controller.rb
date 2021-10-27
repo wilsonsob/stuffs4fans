e39a6b7a32c0de0aa067e3123e49065fa34eeb5e
Wilson Filho <wilsonsenhor@gmail.com>
2021-10-27 12:30:23 -0300

    added columns name and artist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,6 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: :home
+  # skip_before_action :authenticate_user!, only: :home
+  # before_action :configure_permitted_parameters, if: :devise_controller?
   def home
   end
 end
