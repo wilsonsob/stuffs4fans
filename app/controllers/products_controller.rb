5da06da4f897755079028565707ab4b3e5c6fc2a
Luanda Leitao <luanda.leitao@gmail.com>
2021-10-27 18:06:00 -0300

    product-actions-edit-update-destroy

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,22 @@
 class ProductsController < ApplicationController
+before_action :find, only: [:edit, :update, :destroy]
+
+  def edit
+  end
+
+  def update
+    @products.update(products_params)
+    redirect_to products_path(@product)
+  end
+
+  def destroy
+    @product.destroy
+    redirect_to products_path
+  end
+
+  private
+
+  def find
+    @product = Product.find(params[:id])
+  end
 end
