399acdbbc6fa10de56b1bb90d515a379bc068b00
Wilson Filho <wilsonsenhor@gmail.com>
2021-10-27 17:30:35 -0300

    new and create actions added

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,2 +1,21 @@
 class ProductsController < ApplicationController
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+    if @product.save
+      redirect_to products_path(@product)
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:name, :category, :price, :stock, :description)
+  end
 end
