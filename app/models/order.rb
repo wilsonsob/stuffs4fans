a362e494a4a83f2e93cc71b483d3f9d34806d5b6
Wilson Filho <wilsonsenhor@gmail.com>
2021-10-27 15:36:03 -0300

    validates added

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,4 +1,6 @@
 class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
+  validates :quantity, presence: true, numericality: { only_integer: true }
+  # validates :checkbox
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  has_many :orders
+  validates :name, :category, presence: true
+  validates :price, :stock, presence: true, numericality: { only_integer: true }
 end
