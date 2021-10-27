33157f57347e7cd9be78fc235c1cef4d0a06fa18
Luanda Leitao <luanda.leitao@gmail.com>
2021-10-27 20:39:34 -0300

    views iniciais de products, pundit e admin implementados

---
+++ app/policies/product_policy.rb
@@ -0,0 +1,37 @@
+class ProductPolicy < ApplicationPolicy
+  class Scope < Scope
+    def resolve
+      scope.all
+    end
+  end
+
+  def show?
+    true
+  end
+
+  def new?
+    true
+  end
+
+  def create?
+    true
+  end
+
+  def edit?
+    update?
+  end
+
+  def update?
+    is_owner_or_admin?
+  end
+
+  def destroy?
+    is_owner_or_admin?
+  end
+
+  private
+
+  def is_owner_or_admin?
+    record.user == user || user.admin
+  end
+end
