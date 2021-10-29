a2850e202e173f6442095ceaf519a27a248cf94a
Luanda Leitao <luanda.leitao@gmail.com>
2021-10-29 15:36:51 -0300

    create de order funcionando, falta setar quantidade, mas está criando novas orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,7 @@ class Order < ApplicationRecord
   belongs_to :user
   belongs_to :product
   # validates :quantity, presence: true
-  validates :quantity, numericality: { only_integer: true }
+  # validates :quantity, numericality: { only_integer: true }
   validates :product_id, :user_id, presence: true
   # validates :checkbox
 end
