4358427794cf123c9cf3f18a6f18f3572a90fdd3
Luanda Leitao <luanda.leitao@gmail.com>
2021-10-29 12:54:55 -0300

    dispensada autenticacao para index e show de products - ok

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,5 +1,6 @@
 class ProductsController < ApplicationController
 before_action :find, only: [:show, :edit, :update, :destroy]
+skip_before_action :authenticate_user!, only: %i[index show]
 
   def index
     @products = policy_scope(Product)
