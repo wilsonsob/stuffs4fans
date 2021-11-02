93769297e6388b45f922598c5d69db9a1ebebd2d
Luanda Leitao <luanda.leitao@gmail.com>
2021-11-02 08:11:39 -0300

    dispensa de autenticacao para rotas artists e artist

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,5 @@
 class PagesController < ApplicationController
-  skip_before_action :authenticate_user!, only: %i[home]
+  skip_before_action :authenticate_user!, only: %i[home artists artist]
   # before_action :configure_permitted_parameters, if: :devise_controller?
   def home
   end
